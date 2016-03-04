0530b67ec734f40ee636d4fcabce55c7cd1cd066
Mentor <mentor@palokaj.co>
2016-03-04 12:21:37 +0100

    added a block for when bidding ended

--- sniping-script.js
+++ sniping-script.js
@@ -21,6 +21,7 @@ window.setInterval(function(){
 	var nowbid = jQuery('#jsMainLotCurrentBid').text(); // Get the current bid
 	var imwinning = jQuery('#highestBidder').text(); // Get the current highest bidder
 	var timeleft = jQuery ( '.time-value.minutes' ).text (  ); // Get the time left, but only the minutes, not the seconds
+	var timeleft_sec = jQuery ( '.time-value.seconds' ).text (  ); // Get the time left, ins econds
 	var worthtome = 16; // Set the max price I'm willing to pay
 
 	// Update the time we have left
@@ -29,30 +30,39 @@ window.setInterval(function(){
 	// Check if the bidding is in the last minute
 	if  ( timeleft !== 0 ) {
 
-		// Log that we are bidding
-		console.log ( 'We are in the last minute, Im planning to bid' ); 
+		// Check if the seconds are zero, what would mean the bidding is done
+		if  ( timeleft_sec == 0 ) {
 
-		// Update the current time
-		nowbid = Number  ( jQuery('#jsMainLotCurrentBid').text() ) ;
-
-		// Get out the current winning name
-		imwinning = jQuery('#highestBidder').text();
-
-
-		if  ( nowbid < worthtome &&  !( imwinning == myname )  ) {
-			nowbid = nowbid + 1;
-			jQuery ( '#jsActiveBidInput' ).val ( nowbid ); 
-			jQuery ( '#jsActiveBidButton' ).trigger ( "click" );
-			console.log ( 'Bidding succeeded, I bid ' + nowbid ); 
+			// Log that bidding is closed
+			console.log ( "Bidding is closed" ); 
+			
 		} else {
-			if  ( !( imwinning == myname )  ) {
-				console.log ( "Not bidding because we hold the highest bid" ); 
+				// Log that we are bidding
+				console.log ( 'We are in the last minute, Im planning to bid' ); 
+
+				// Update the current time
+				nowbid = Number  ( jQuery('#jsMainLotCurrentBid').text() ) ;
+
+				// Get out the current winning name
+				imwinning = jQuery('#highestBidder').text();
+
+
+				if  ( nowbid < worthtome &&  !( imwinning == myname )  ) {
+					nowbid = nowbid + 1;
+					jQuery ( '#jsActiveBidInput' ).val ( nowbid ); 
+					jQuery ( '#jsActiveBidButton' ).trigger ( "click" );
+					console.log ( 'Bidding succeeded, I bid ' + nowbid ); 
+				} else {
+					if  ( !( imwinning == myname )  ) {
+						console.log ( "Not bidding because we hold the highest bid" ); 
+					}
+					if  ( nowbid > worthtome ) {
+						console.log ( "Not bidding because the bid is higher than out max" ); 
+					} 
+				}
 			}
-			if  ( nowbid > worthtome ) {
-				console.log ( "Not bidding because the bid is higher than out max" ); 
-			} 
-		}
-	} else {
+
+		} else {
 		// Log that we are not bidding yet
 		console.log ( 'Too much time left, not bidding' ); 
 	}
