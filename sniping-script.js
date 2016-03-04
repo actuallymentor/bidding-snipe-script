b063d1b309aa7e9368b98c38b8d5697f48757bf2
Mentor <mentor@palokaj.co>
2016-03-04 12:10:43 +0100

    better fucntion structure and console feedback

--- sniping-script.js
+++ sniping-script.js
@@ -1,34 +1,42 @@
 var jq = document.createElement('script');
 jq.src = "//code.jquery.com/jquery-latest.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
+var jqloaded = false;
 
 var isjq =  document.getElementsByTagName("head");
 jq.onload = function() {
-    jQuery.noConflict();
-    console.log ( 'jQ loaded' ); 
+	jQuery.noConflict();
+	console.log ( 'jQ loaded' );
+	jqloaded = true;
+
 };
 
-// Create needed variables
-var myname = "Mentor Palokaj"; // Set my name
-var nowbid = jQuery('#jsMainLotCurrentBid').text(); // Get the current bid
-var imwinning = jQuery('#highestBidder').text(); // Get the current highest bidder
-var timeleft = jQuery ( '.time-value.minutes' ).text (  ); // Get the time left, but only the minutes, not the seconds
-var worthtome = 20; // Set the max price I'm willing to pay
-var biddingspeed = 5000; // Bid every how many milliseconds?
+
+
+
 
 window.setInterval(function(){
+
+	if  ( jqloaded )  {
+	// Create needed variables
+	var myname = "Mentor Palokaj"; // Set my name
+	var nowbid = jQuery('#jsMainLotCurrentBid').text(); // Get the current bid
+	var imwinning = jQuery('#highestBidder').text(); // Get the current highest bidder
+	var timeleft = jQuery ( '.time-value.minutes' ).text (  ); // Get the time left, but only the minutes, not the seconds
+	var worthtome = 16; // Set the max price I'm willing to pay
+	var biddingspeed = 5000; // Bid every how many milliseconds?
 
 	// Update the time we have left
 	timeleft = jQuery ( '.time-value.minutes' ).text (  );
 
 	// Check if the bidding is in the last minute
-	if  ( timeleft == 0 ) {
+	if  ( timeleft !== 0 ) {
 
 		// Log that we are bidding
-		console.log ( 'We are in the last minute, Im bidding' ); 
+		console.log ( 'We are in the last minute, Im planning to bid' ); 
 
 		// Update the current time
-		nowbid = jQuery('#jsMainLotCurrentBid').text();
+		nowbid = Number  ( jQuery('#jsMainLotCurrentBid').text() ) ;
 
 		// Get out the current winning name
 		imwinning = jQuery('#highestBidder').text();
@@ -39,11 +47,17 @@ window.setInterval(function(){
 			jQuery ( '#jsActiveBidInput' ).val ( nowbid ); 
 			jQuery ( '#jsActiveBidButton' ).trigger ( "click" );
 			console.log ( 'Bidding succeeded, I bid ' + nowbid ); 
+		} else ( ) {
+			if  ( !( imwinning == myname )  ) {
+				console.log ( "Not bidding because we hold the highest bid" ); 
+			}
+			if  ( nowbid > worthtome ) {
+				console.log ( "Not bidding because the bid is higher than out max" ); 
+			} 
 		}
 	} else {
 		// Log that we are not bidding yet
 		console.log ( 'Too much time left, not bidding' ); 
 	}
+}
 }, 5000);
-
-
