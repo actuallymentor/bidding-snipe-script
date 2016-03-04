e90103f1450f60840ab6f9fac6037bbb9b1c22a9
Mentor <mentor@palokaj.co>
2016-03-04 11:17:10 +0100

    initial updload of doodling

---
+++ sniping-script.js
@@ -0,0 +1,36 @@
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+jQuery.noConflict();
+
+// Create needed variables
+var myname = "Mentor Palokaj"; // Set my name
+var nowbid = $('#jsMainLotCurrentBid').text(); // Get the current bid
+var imwinning = $('#highestBidder').text(); // Get the current highest bidder
+var timeleft = $ ( '.time-value.minutes' ).text (  ); // Get the time left, but only the minutes, not the seconds
+var worthtome = 20; // Set the max price I'm willing to pay
+var biddingspeed = 5000; // Bid every how many milliseconds?
+
+window.setInterval(function(){
+
+	// Update the time we have left
+	timeleft = $ ( '.time-value.minutes' ).text (  );
+
+	// Check if the bidding is in the last minute
+	if  ( timeleft == 0 ) {
+
+		// Log that we are bidding
+		console.log ( 'We are in the last minute, Im bidding' ); 
+		nowbid = $('#jsMainLotCurrentBid').text();
+		imwinning = $('#highestBidder').text();
+		if  ( nowbid < worthtome &&  !( imwinning == myname )  ) {
+			$ ( '#jsActiveBidInput' ).val ( nowbid ); 
+			$ ( '#jsActiveBidButton' ).trigger ( "click" );
+			console.log ( 'Bidding succeeded, I bid ' + worthtome ); 
+		}
+	} else {
+		// Log that we are not bidding yet
+		console.log ( 'Too much time left, not bidding' ); 
+	}
+}, 5000);
