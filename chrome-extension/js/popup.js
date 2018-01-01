93dace8afb5b313a13c77ec514753230a003a890
Mentor Palokaj <mentor@palokaj.co>
2018-01-01 13:44:59 +0100

    Commentify

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -19,19 +19,28 @@ const menu = {
   }
 }
 
+// Initialise the interface
 chrome.storage.sync.get( menu.storage.def , item => {
+  // Set the bidding status based on the stored status
   item[ 'vv_bidding' ] ? set( menu.bid.display, 'active' ) : set( menu.status.id, 'inactive' )
+  // Set the interface bid based on the stored bid
   set( menu.bid.display, item[ 'vv_bid' ] ?  item[ 'vv_bid' ] : 'not set' )
 } )
 
+
+// Activation button logic
 click( 'activate', f => { 
-  console.log( 'Started bidding' )
+  // Set the storage to active and store the bidding amount
   chrome.storage.sync.set( { vv_bidding: true, vv_bid: id( menu.bid.input ).value }, f => { 
+    // Show interface as active
     set( menu.status.id, 'active' )
+    // Get the stored bid and display it
     chrome.storage.sync.get( menu.storage.bid, item => set( menu.bid.display, item[ 'vv_bid' ] ) )
   } )
 } )
 
+// Deactivatio logic
 click( 'deactivate', f => { 
+  // Set the bidding status to inactive and show it in the interface
   chrome.storage.sync.set( { vv_bidding: false }, item => set( menu.status.id, 'inactive' ) )
 } )
